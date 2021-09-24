dd2ddff35edd6ac84fe6bc540f938df910eb0ee6
Lucas Orsini <lucasorsini@gmail.com>
2021-09-23 22:09:04 -0300

    final

--- main.js
+++ main.js
@@ -6,18 +6,18 @@ let porcentajeVotosB = 0;
 let porcentajeBlancos = 0;
  
 
-for (i=1; i<=5; i++) {
-    let votaPor = parseInt(prompt("Por que partiodo desea votar:\n 1-Partido A\n 2-Partido B\n 3-Voto Blanco"));
+for (i=1; i<=10; i++) {
+    let votaPor = parseInt(prompt("Por que partido desea votar:\n 1-Partido A\n 2-Partido B\n 3-Voto Blanco"));
     
         if (votaPor == 1) {
             votoA = votoA+1;
-            porcentajeVotosA = parseFloat(votoA*100/5);
+            porcentajeVotosA = parseFloat(votoA*100/10);
         }else if (votaPor == 2) {
             votoB = votoB + 1;
-            porcentajeVotosB = votoB*100/5;
+            porcentajeVotosB = parseFloat(votoB*100/10);
         }else if (votaPor == 3) {
             votoBlanco = votoBlanco + 1;
-            porcentajeBlancos = votoBlanco*100/5;
+            porcentajeBlancos = parseFloat(votoBlanco*100/10);
         }
         else {
             alert("Su voto no es valido");
@@ -30,11 +30,11 @@ for (i=1; i<=5; i++) {
 
 
 if (votoA > votoB) {
-    alert("El ganador es el paartido A \n" + "Partido A: " + porcentajeVotosA + " % de los votos. \n" + "Partido B: " + porcentajeVotosB + "% de lo votos \n" + "Votos en blanco: " + porcentajeBlancos + "% ");
+    alert("El ganador es el partido A \n" + "Partido A: " + porcentajeVotosA + " % de los votos. \n" + "Partido B: " + porcentajeVotosB + "% de lo votos \n" + "Votos en blanco: " + porcentajeBlancos + "% ");
 }else if (votoA == votoB) {
     alert("Empate.. al balotage\n" + "Partido A: " + porcentajeVotosA + "% de los votos. \n" + "Partido B: " + porcentajeVotosB + "% de lo votos \n" + "Votos en blanco: " + porcentajeBlancos + "% ");
 }else {
-    alert("El ganador es el paartido B \n" + "Partido B: " + porcentajeVotosB + "% de los votos. \n" + "Partido A: " + porcentajeVotosA + "% de lo votos \n" + "Votos en blanco: " + porcentajeBlancos + "% ");
+    alert("El ganador es el partido B \n" + "Partido B: " + porcentajeVotosB + "% de los votos. \n" + "Partido A: " + porcentajeVotosA + "% de lo votos \n" + "Votos en blanco: " + porcentajeBlancos + "% ");
 }
 
 
